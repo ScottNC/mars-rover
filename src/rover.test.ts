9414b59c7c328bce5218418ab751e8f93a8b9bc6
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-06-10 18:32:12 +0100

    tests turning in all directions

--- src/rover.test.ts
+++ src/rover.test.ts
@@ -4,11 +4,14 @@ describe("runRovers", () => {
     expect(runRovers(['1 1', '0 0 N', 'M'])).toEqual(['0 1 N']);
   });
 
-  it("should turn left", () => {
+  it("should turn left and right in all directions", () => {
     expect(runRovers(['1 1', '0 0 N', 'L'])).toEqual(['0 0 W']);
-  });
-
-  it("should turn right", () => {
+    expect(runRovers(['1 1', '0 0 W', 'L'])).toEqual(['0 0 S']);
+    expect(runRovers(['1 1', '0 0 S', 'L'])).toEqual(['0 0 E']);
+    expect(runRovers(['1 1', '0 0 E', 'L'])).toEqual(['0 0 N']);
     expect(runRovers(['1 1', '0 0 N', 'R'])).toEqual(['0 0 E']);
+    expect(runRovers(['1 1', '0 0 E', 'R'])).toEqual(['0 0 S']);
+    expect(runRovers(['1 1', '0 0 S', 'R'])).toEqual(['0 0 W']);
+    expect(runRovers(['1 1', '0 0 W', 'R'])).toEqual(['0 0 N']);
   });
 });
